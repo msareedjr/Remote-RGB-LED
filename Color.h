570971a201824baa67136819d4392e00420c8733
Michael Reed Jr <michael@michael-reed.com>
2017-05-07 13:57:20 -0400

    Moved static definitions out of header file.

--- Color.h
+++ Color.h
@@ -8,32 +8,13 @@ class Color
   uint8_t red;
   uint8_t green;
   uint8_t blue;
+  
   void setRGB( uint8_t red, uint8_t green, uint8_t blue);
+  
   Color(  uint8_t red, uint8_t green, uint8_t blue);
-  bool operator == (Color &arg)
-  {
-    return (this.red == arg.red) && (this.green == arg.green) && (this.blue == arg.blue);
-  }
-  bool operator != (Color &arg)
-  {
-    return (this.red != arg.red) || (this.green != arg.green) || (this.blue != arg.blue);
-  }
-  static Color red = Color(255, 0, 0);
-  static Color red_orange = Color(255, 63, 52);
-  static Color orange = Color(255, 104, 31);
-  static Color yellow_orange = Color(255, 174, 66);
-  static Color yellow = Color(255, 255, 0);
-  static Color yellow_green = Color(197, 225, 122);
-  static Color green = Color(0, 255, 0);
-  static Color caribbean_green = Color(0, 209, 155);
-  static Color cyan = Color(0, 255, 255);
-  static Color java = Color(31, 194, 194);
-  static Color blue = Color(0, 0, 255);
-  static Color blue_violet = Color(92, 0, 255);
-  static Color purple = Color(175, 15, 255);
-  static Color magenta = Color(255, 0, 255);
-  static Color rose = Color(255, 0, 127);
-  static Color white = Color(255, 255, 255);
+  
+  bool operator == (Color &arg);
+  bool operator != (Color &arg);
   
 }
 #endif
