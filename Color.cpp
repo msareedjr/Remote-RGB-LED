377a0b131879bd450251fd8d3bd99b78dcd71665
Michael Reed Jr <michael@michael-reed.com>
2017-05-10 20:39:08 -0400

    Added GPLv3 license to project

--- Color.cpp
+++ Color.cpp
@@ -5,6 +5,17 @@
  * Version 0.1 May, 2017
  * Copyright 2017 Michael Reed 
  * https://michael-reed.com
+ *    
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
   
   Color::Color(  uint8_t red, uint8_t green, uint8_t blue)
--- remoteLED.ino
+++ remoteLED.ino
@@ -3,8 +3,18 @@
  * and a remote control that came with a RGB light bulb.
  * 
  * Version 0.1 May, 2017
- * Copyright 2017 Michael Reed 
- * https://michael-reed.com
+ * Copyright (C) 2017 Michael Reed https://michael-reed.com
+ * 
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 // Red LED pin
